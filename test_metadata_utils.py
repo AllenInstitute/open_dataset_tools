9af035906e2faeb3b5bd04fea6f70c780184ff22
danielsf <scott.daniel@alleninstitute.org>
2020-05-11 17:33:57 -0700

    test metadata download with session=None

--- test_metadata_utils.py
+++ test_metadata_utils.py
@@ -48,28 +48,29 @@ class MetadataTestCase(unittest.TestCase):
         file. Compare its md5 checksum to a hard-coded value taken
         from a verified copy that was downloaded from S3 by hand.
         """
-        metadata = mu.get_atlas_metadata(session=self.session,
-                                         tmp_dir=self.tmp_dir)
+        for session in (self.session, None):
+            metadata = mu.get_atlas_metadata(session=session,
+                                             tmp_dir=self.tmp_dir)
 
-        self.assertIsInstance(metadata, list)
-        self.assertEqual(len(metadata), 26078)
-        for obj in metadata:
-            self.assertIsInstance(obj, dict)
+            self.assertIsInstance(metadata, list)
+            self.assertEqual(len(metadata), 26078)
+            for obj in metadata:
+                self.assertIsInstance(obj, dict)
 
-        fname = os.path.join(self.tmp_dir, 'section_data_sets.json')
+            fname = os.path.join(self.tmp_dir, 'section_data_sets.json')
 
-        if not os.path.exists(os.path.join(fname)):
-            raise RuntimeError("Failed to download section_data_sets.json")
+            if not os.path.exists(os.path.join(fname)):
+                raise RuntimeError("Failed to download section_data_sets.json")
 
-        md5_obj = hashlib.md5()
-        with open(os.path.join(fname), 'rb') as in_file:
-            for line in in_file:
-                md5_obj.update(line)
-        checksum = md5_obj.hexdigest()
-        self.assertEqual(checksum,
-                         '2c974e2be3a30a4d923f47dd4a7fde72')
+            md5_obj = hashlib.md5()
+            with open(os.path.join(fname), 'rb') as in_file:
+                for line in in_file:
+                    md5_obj.update(line)
+            checksum = md5_obj.hexdigest()
+            self.assertEqual(checksum,
+                             '2c974e2be3a30a4d923f47dd4a7fde72')
 
-        os.unlink(fname)
+            os.unlink(fname)
 
     def test_section_metadata(self):
         """
@@ -77,27 +78,28 @@ class MetadataTestCase(unittest.TestCase):
         against a hard-coded md5 checksum
         """
         section_id = 99
-        metadata = mu.get_section_metadata(section_id=section_id,
-                                           session=self.session,
-                                           tmp_dir=self.tmp_dir)
+        for session in (self.session, None):
+            metadata = mu.get_section_metadata(section_id=section_id,
+                                               session=session,
+                                               tmp_dir=self.tmp_dir)
 
-        self.assertIsInstance(metadata, dict)
+            self.assertIsInstance(metadata, dict)
 
-        fname = os.path.join(self.tmp_dir,
-                             'section_data_set_%d_metadata.json' % section_id)
+            fname = os.path.join(self.tmp_dir,
+                                 'section_data_set_%d_metadata.json' % section_id)
 
-        if not os.path.exists(os.path.join(fname)):
-            raise RuntimeError("Failed to download %s" % fname)
+            if not os.path.exists(os.path.join(fname)):
+                raise RuntimeError("Failed to download %s" % fname)
 
-        md5_obj = hashlib.md5()
-        with open(os.path.join(fname), 'rb') as in_file:
-            for line in in_file:
-                md5_obj.update(line)
-        checksum = md5_obj.hexdigest()
-        self.assertEqual(checksum,
-                         'e8eff384bb39cc981f93bad62e6fad02')
+            md5_obj = hashlib.md5()
+            with open(os.path.join(fname), 'rb') as in_file:
+                for line in in_file:
+                    md5_obj.update(line)
+            checksum = md5_obj.hexdigest()
+            self.assertEqual(checksum,
+                             'e8eff384bb39cc981f93bad62e6fad02')
 
-        os.unlink(fname)
+            os.unlink(fname)
 
 if __name__ == "__main__":
     unittest.main()
